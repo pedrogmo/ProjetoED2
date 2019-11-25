fd4dd280a07d478e44afda945208dda5bffeec81
GustavoHMeira <cc18015@g.unicamp.br>
2019-11-25 13:11:20 -0300

    falta coisa

--- 18015_18174_ProjetoED/TrensCidadesAndroid/app/src/main/java/com/example/trenscidadesandroid/AdicionarCidade.java
+++ 18015_18174_ProjetoED/TrensCidadesAndroid/app/src/main/java/com/example/trenscidadesandroid/AdicionarCidade.java
@@ -1,14 +1,76 @@
 package com.example.trenscidadesandroid;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Xml;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AdicionarCidade extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
+public class AdicionarCidade extends AppCompatActivity {
+    EditText etNome, etCoordenadaX, etCoordenadaY;
+    Button btnAdicionar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_cidade);
+
+        etNome = findViewById(R.id.etNome);
+        etCoordenadaX = findViewById(R.id.etCoordenadaX);
+        etCoordenadaY = findViewById(R.id.etCoordenadaY);
+
+        AssetManager assetManager = getApplicationContext().getAssets();
+        InputStream inputStream;
+
+        bhCidade = new BucketHash<Cidade>();
+
+        try {
+            inputStream = assetManager.open("cidades.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String recebe_string;
+            while((recebe_string = bufferedReader.readLine())!=null){
+
+                Cidade cd = new Cidade(new Linha(recebe_string));
+                bhCidade.inserir(cd);
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+        btnAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!etNome.getText().toString().trim().equals("")  && !etCoordenadaX.getText().toString().trim().equals("")
+                        && etCoordenadaY.getText().toString().trim().equals(""))
+                {
+                    Cidade cd = new Cidade(etNome.getText().toString().trim());
+                    if (bhCidade.Buscar(cd) == null)
+                    {
+                        StreamWriter sw = new StreamWriter(assets.Open("cidades.txt"));
+                        cd.X = Double.Parse(etCoordenadaX.Text.Trim());
+                        cd.Y = Double.Parse(etCoordenadaY.Text.Trim());
+                        cd.Codigo = bhCidade.Quantidade;
+                        sw.Write(cd.ParaArquivo());
+                        sw.Close();
+                    }
+                    else
+                        Toast.MakeText(Application.Context, "Essa cidade já existe", ToastLength.Short);
+                }
+                else
+                    Toast.MakeText(Application.Context, "Há campos vazios", ToastLength.Short);
+            }
+        });
     }
-}
+    }
+
--- 18015_18174_ProjetoED/TrensCidadesAndroid/app/src/main/java/com/example/trenscidadesandroid/MainActivity.java
+++ 18015_18174_ProjetoED/TrensCidadesAndroid/app/src/main/java/com/example/trenscidadesandroid/MainActivity.java
@@ -1,14 +1,80 @@
 package com.example.trenscidadesandroid;
 
+import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Xml;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TableLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spDeOnde, spParaOnde;
+    View canvasView;
+    LinearLayout layoutCanvas;
+    Button btnBuscar, btnAdicionarCidade, btnAdicionarCaminho;
+    TableLayout tbCaminhos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spDeOnde = findViewById(R.id.spDeOnde);
+        spParaOnde = findViewById(R.id.spParaOnde);
+        btnBuscar = findViewById(R.id.btnBuscar);
+        btnAdicionarCidade = findViewById(R.id.btnAdicionarCidade);
+        btnAdicionarCaminho = findViewById(R.id.btnAdicionarCaminho);
+
+        canvasView = new View(this);
+        layoutCanvas.addView(canvasView);
+
+        bhCidade = new BucketHash<Cidade>();
+        ArrayAdapter<String> cidadesSpinner = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1);
+
+        AssetManager assetManager = getResources().getAssets();
+        InputStream inputStream;
+
+        try {
+            inputStream = assetManager.open("cidades.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String recebe_string;
+            while((recebe_string = bufferedReader.readLine())!=null){
+
+                Cidade cd = new Cidade(new Linha(recebe_string));
+                cidadesSpinner.add(cd.toString());
+                bhCidade.inserir(cd);
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        spDeOnde.setAdapter(cidadesSpinner);
+        spParaOnde.setAdapter(cidadesSpinner);
+
+        btnAdicionarCidade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), AdicionarCidade.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
